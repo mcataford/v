bf9d1a31c5f3fbd7a62af0b37e6e35225313fef1
Marc Cataford <mcat@riseup.net>
2023-12-03 23:51:54 -0500

    test: which coverage

--- commands_test.go
+++ commands_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -57,5 +58,74 @@ func TestListVersionReturnsErrorOnFailure(t *testing.T) {
 
 	if err == nil {
 		t.Errorf("Expected error returned, did not get one.")
+	}
+}
+
+func TestListVersionOutputsVersionSelectedAndWarnsNotInstalled(t *testing.T) {
+	defer SetupAndCleanupEnvironment(t)()
+
+	var out bytes.Buffer
+
+	InfoLogger.SetOutput(&out)
+	defer InfoLogger.SetOutput(os.Stdout)
+
+	Which([]string{}, Flags{}, State{GlobalVersion: "1.2.3"})
+
+	captured := out.String()
+	if captured != "The desired version (1.2.3) is not installed.\n" {
+		t.Errorf("Unexpected message: %s", captured)
+	}
+}
+
+func TestWhichOutputsVersionSelectedIfInstalled(t *testing.T) {
+	defer SetupAndCleanupEnvironment(t)()
+
+	var out bytes.Buffer
+
+	InfoLogger.SetOutput(&out)
+	defer InfoLogger.SetOutput(os.Stdout)
+
+	os.MkdirAll(GetStatePath("runtimes", "py-1.2.3"), 0750)
+	Which([]string{}, Flags{}, State{GlobalVersion: "1.2.3"})
+
+	captured := strings.TrimSpace(out.String())
+	expected := GetStatePath("runtimes", "py-1.2.3", "bin", "python1.2")
+	if !strings.Contains(captured, expected) {
+		t.Errorf("Unexpected message: %s, not %s", captured, expected)
+	}
+}
+
+func TestWhichOutputsSystemVersionIfNoneSelected(t *testing.T) {
+	defer SetupAndCleanupEnvironment(t)()
+
+	var out bytes.Buffer
+
+	InfoLogger.SetOutput(&out)
+	defer InfoLogger.SetOutput(os.Stdout)
+
+	Which([]string{}, Flags{RawOutput: true}, State{})
+
+	captured := strings.TrimSpace(out.String())
+
+	if captured != "/bin/python (system)" {
+		t.Errorf("%s != %s", captured, "/bin/python (system)")
+	}
+}
+
+func TestWhichOutputsVersionWithoutPrefixesIfRawOutput(t *testing.T) {
+	defer SetupAndCleanupEnvironment(t)()
+
+	var out bytes.Buffer
+
+	InfoLogger.SetOutput(&out)
+	defer InfoLogger.SetOutput(os.Stdout)
+
+	os.MkdirAll(GetStatePath("runtimes", "py-1.2.3"), 0750)
+	Which([]string{}, Flags{RawOutput: true}, State{GlobalVersion: "1.2.3"})
+
+	captured := strings.TrimSpace(out.String())
+	expected := GetStatePath("runtimes", "py-1.2.3", "bin", "python1.2")
+	if captured != expected {
+		t.Errorf("Unexpected message: %s, not %s", captured, expected)
 	}
 }
