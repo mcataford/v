d1200b81d17c3ef0b1cd4367e96becc1648a5892
Marc Cataford <mcat@riseup.net>
2024-01-26 00:27:46 -0500

    feat(cli): separate python commands into own namespace

--- v.go
+++ v.go
@@ -18,27 +18,35 @@ func main() {
 	args := os.Args[1:]
 	currentState := state.ReadState()
 
+	root := cli.Namespace{Label: ""}
+	root.AddCommand(
+		"init", Initialize, "v init", "Initializes the v state.",
+	)
+
+	pythonCommands := cli.Namespace{Label: "python"}
+	pythonCommands.AddCommand(
+		"install", python.InstallPython, "v python install <version>", "Downloads, builds and installs a new version of Python.",
+	).AddCommand(
+		"uninstall", python.UninstallPython, "v python uninstall <version>", "Uninstalls the given Python version.",
+	).AddCommand(
+		"use", python.Use, "v python use <version>", "Selects which Python version to use.",
+	).AddCommand(
+		"ls", python.ListVersions, "v python ls", "Lists the installed Python versions.",
+	).AddCommand(
+		"version", python.CurrentVersion, "v python version", "Prints the current version and its source.",
+	).AddCommand(
+		"which", python.Which, "v python which", "Prints the path to the current Python version.",
+	)
+
 	cli := cli.CLI{
 		Metadata: map[string]string{
 			"Version": Version,
 		},
 	}
 
-	err := cli.AddCommand(
-		"install", python.InstallPython, "v install <version>", "Downloads, builds and installs a new version of Python.",
-	).AddCommand(
-		"uninstall", python.UninstallPython, "v uninstall <version>", "Uninstalls the given Python version.",
-	).AddCommand(
-		"use", python.Use, "v use <version>", "Selects which Python version to use.",
-	).AddCommand(
-		"ls", python.ListVersions, "v ls", "Lists the installed Python versions.",
-	).AddCommand(
-		"version", python.CurrentVersion, "v version", "Prints the current version and its source.",
-	).AddCommand(
-		"which", python.Which, "v which", "Prints the path to the current Python version.",
-	).AddCommand(
-		"init", Initialize, "v init", "Initializes the v state.",
-	).Run(args, currentState)
+	cli.AddNamespace(root).AddNamespace(pythonCommands)
+
+	err := cli.Run(args, currentState)
 
 	if err != nil {
 		panic(err)
